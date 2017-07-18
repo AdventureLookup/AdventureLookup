0d0516e885c9b24b2de61283f3d20a4977a01684
Christian Flach <cmfcmf.flach@gmail.com>
2017-07-18 10:11:45 +0200

    Convert info fields into named fields

--- src/AppBundle/Entity/AdventureDocument.php
+++ src/AppBundle/Entity/AdventureDocument.php
@@ -15,6 +15,101 @@ class AdventureDocument
 
     private $info;
 
+    /**
+     * @var string
+     */
+    private $system;
+
+    /**
+     * @var string
+     */
+    private $publisher;
+
+    /**
+     * @var string
+     */
+    private $setting;
+
+    /**
+     * @var integer
+     */
+    private $minStartingLevel;
+
+    /**
+     * @var integer
+     */
+    private $maxStartingLevel;
+
+    /**
+     * @var integer
+     */
+    private $levelRange;
+
+    /**
+     * @var boolean
+     */
+    private $soloable;
+
+    /**
+     * @var integer
+     */
+    private $numPages;
+
+    /**
+     * @var boolean
+     */
+    private $pregeneratedCharacters;
+
+    /**
+     * @var string[]
+     */
+    private $environments;
+
+    /**
+     * @var string
+     */
+    private $link;
+
+    /**
+     * @var string
+     */
+    private $thumbnailUrl;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string[]
+     */
+    private $notableItems;
+
+    /**
+     * @var string[]
+     */
+    private $monsters;
+
+    /**
+     * @var boolean
+     */
+    private $tacticalMaps;
+
+    /**
+     * @var boolean
+     */
+    private $handouts;
+
+    /**
+     * @var string[]
+     */
+    private $villains;
+
+    /**
+     * @var string
+     */
+    private $foundIn;
+
     public function __construct(int $id, string $title, string $slug, array $info, float $score = 0.0)
     {
         $this->id = $id;
@@ -22,6 +117,37 @@ class AdventureDocument
         $this->slug = $slug;
         $this->score = $score;
         $this->info = $info;
+
+        $map = [
+            'Author' => 'author',
+            'System / Edition' => 'system',
+            'Publisher' => 'publisher',
+            'Setting' => 'setting',
+            'Min. Starting Level' => 'minStartingLevel',
+            'Max. Starting Level' => 'maxStartingLevel',
+            'Level Range' => 'levelRange',
+            'Suitable for Solo Play' => 'soloable',
+            'Length (# of Pages)' => 'numPages',
+            'Includes Pregenerated Characters' => 'pregeneratedCharacters',
+            'Environment' => 'environments',
+            'Link' => 'link',
+            'Thumbnail' => 'thumbnailUrl',
+            'Description' => 'description',
+            'Notable Items' => 'notableItems',
+            'Monsters' => 'monsters',
+            'Tactical Maps' => 'tacticalMaps',
+            'Handouts' => 'handouts',
+            'Villains' => 'villains',
+            'Found in ' => 'foundIn'
+        ];
+
+        foreach ($info as $infoObj) {
+            $fieldTitle = $infoObj['meta']->getTitle();
+            if (isset($map[$fieldTitle])) {
+                $fieldName = $map[$fieldTitle];
+                $this->$fieldName = $infoObj['contents'];
+            }
+        }
     }
 
     public static function fromAdventure(Adventure $adventure)
@@ -76,8 +202,160 @@ class AdventureDocument
     /**
      * @return string
      */
-    public function getSlug(): string
+    public function getSlug()
     {
         return $this->slug;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSystem()
+    {
+        return $this->system;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublisher()
+    {
+        return $this->publisher;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSetting()
+    {
+        return $this->setting;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMinStartingLevel()
+    {
+        return $this->minStartingLevel;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMaxStartingLevel()
+    {
+        return $this->maxStartingLevel;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLevelRange()
+    {
+        return $this->levelRange;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSoloable()
+    {
+        return $this->soloable;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumPages()
+    {
+        return $this->numPages;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPregeneratedCharacters()
+    {
+        return $this->pregeneratedCharacters;
+    }
+
+    /**
+     * @return \string[]
+     */
+    public function getEnvironments()
+    {
+        return $this->environments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * @return string
+     */
+    public function getThumbnailUrl()
+    {
+        return $this->thumbnailUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return \string[]
+     */
+    public function getNotableItems()
+    {
+        return $this->notableItems;
+    }
+
+    /**
+     * @return \string[]
+     */
+    public function getMonsters()
+    {
+        return $this->monsters;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTacticalMaps()
+    {
+        return $this->tacticalMaps;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHandouts()
+    {
+        return $this->handouts;
+    }
+
+    /**
+     * @return \string[]
+     */
+    public function getVillains()
+    {
+        return $this->villains;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFoundIn()
+    {
+        return $this->foundIn;
     }
 }
