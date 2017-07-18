0cf25913aecc887b8ba2fcc5ff8bc7e5c8c1c063
Robert Burnham <burnhamrobertp@gmail.com>
2017-07-17 23:39:08 -0500

    Some slight improvements to README, fix webpack scripts
    You can now just do `npm run dev` in order to run webpack -w -p

--- webpack.config.js
+++ webpack.config.js
@@ -3,61 +3,64 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: './app/Resources/webpack/index.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'web/assets')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: [/node_modules/],
-        use: [{
-          loader: 'babel-loader',
-          options: { presets: ['es2015'] }
-        }]
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            'css-loader',
-            'postcss-loader',
-            'sass-loader',
-          ]
-        })
-      },
-      // the url-loader uses DataUrls.
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff&publicPath=/assets/' },
-      // the file-loader emits files.
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?publicPath=/assets/' },
-      { test: /\.(png|jpeg|jpg|gif)$/, loader: 'file-loader?publicPath=/assets/' },
-    ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      Tether: "tether",
-    }),
-    new ExtractTextPlugin('styles.css'),
-    //new webpack.optimize.CommonsChunkPlugin({
-    //  name: 'commons',
-    //  filename: 'commons.js',
-    //  minChunks: 2,
-    //}),
-  ],
-  resolve: {
-    alias: {
-      jquery: "jquery/src/jquery",
-      tether: "tether/dist/js/tether.js",
-    }
-  },
-  watchOptions: {
-    poll: true
-  },
+    entry: {
+        main: './app/Resources/webpack/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'web/assets')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: {presets: ['es2015']}
+                }]
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader',
+                    ]
+                })
+            },
+            // the url-loader uses DataUrls.
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff&publicPath=/assets/'
+            },
+            // the file-loader emits files.
+            {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?publicPath=/assets/'},
+            {test: /\.(png|jpeg|jpg|gif)$/, loader: 'file-loader?publicPath=/assets/'},
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            Tether: "tether",
+        }),
+        new ExtractTextPlugin('styles.css'),
+        //new webpack.optimize.CommonsChunkPlugin({
+        //  name: 'commons',
+        //  filename: 'commons.js',
+        //  minChunks: 2,
+        //}),
+    ],
+    resolve: {
+        alias: {
+            jquery: "jquery/src/jquery",
+            tether: "tether/dist/js/tether.js",
+        }
+    },
+    watchOptions: {
+        poll: true
+    },
 };
