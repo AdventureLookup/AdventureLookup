0b7a4687ae4f15405da93d8059db0e835c2b0fa5
Christian Flach <cmfcmf.flach@gmail.com>
2017-07-22 22:19:39 +0200

    Fix search tags for integer filters

--- app/Resources/webpack/search.js
+++ app/Resources/webpack/search.js
@@ -14,11 +14,14 @@ import 'nouislider/distribute/nouislider.css';
     $('.filter-tag .filter-remove').click(function () {
         const fieldName = $(this).data('field-name');
         const value = $(this).data('value');
+        const key = $(this).data('key');
         const fieldType = $(this).data('field-type');
         if (fieldType === 'string') {
             $(`input[name^="f[${fieldName}][v]"][value="${value}"]`).prop('checked', false);
         } else if (fieldType === 'boolean') {
             $(`input[name^="f[${fieldName}][v]"][value=""]`).prop('checked', true);
+        } else if (fieldType === 'integer') {
+            $(`input[name^="f[${fieldName}][v][${key}]"]`).val('');
         }
         $(this).parent('.filter-tag').remove();
         $('#search-btn').click();
