4ac61aa79f1ef2d27f4c5cedc20bcb46a7a28b15
Christian Flach <cmfcmf.flach@gmail.com>
2017-08-01 11:47:08 +0200

    Upgrade to Symfony 3.3

--- scripts/start-phantomjs.sh
+++ scripts/start-phantomjs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -ev
-php bin/console server:start -p 8001 --env test --no-debug --force
+php bin/console server:start 8001 --env test --pidfile /tmp/adl-test-server.pid
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /tmp/gastonjs.log &
