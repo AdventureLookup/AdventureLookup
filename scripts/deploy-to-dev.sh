351c9bc62cc0d598d61f0369adbd21af40fb2629
Christian Flach <cmfcmf.flach@gmail.com>
2017-08-08 17:04:30 +0200

    Add host key instead of removing strict host key checking

--- scripts/deploy-to-dev.sh
+++ scripts/deploy-to-dev.sh
@@ -2,6 +2,4 @@
 # Can be called from the application's root directory to deploy the latest changes from GitHub to the dev server.
 # Assumes the SSH_PORT is set in an environment variable.
 
-set -e
-echo -e "Host dev.adventurelookup.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh deploy@dev.adventurelookup.com -p ${SSH_PORT} 'set -ev; cd /var/www/dev.adventurelookup.com/html/; bash -s "dev";' < ./scripts/update.sh
