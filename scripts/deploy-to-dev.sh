65ebe0f5fe6ecff2c7b2950fe5077d507f599d80
Christian Flach <cmfcmf.flach@gmail.com>
2017-08-16 20:58:46 +0200

    Use git reset --hard to pull changes, while overwriting local changes

--- scripts/deploy-to-dev.sh
+++ scripts/deploy-to-dev.sh
@@ -2,4 +2,4 @@
 # Can be called from the application's root directory to deploy the latest changes from GitHub to the dev server.
 # Assumes the SSH_PORT is set in an environment variable.
 
-ssh deploy@dev.adventurelookup.com -p ${SSH_PORT} 'set -ev; cd /var/www/dev.adventurelookup.com/html/; bash -s "dev";' < ./scripts/update.sh
+ssh deploy@dev.adventurelookup.com -p ${SSH_PORT} 'set -ev; cd /var/www/dev.adventurelookup.com/html/; bash -s "origin/dev";' < ./scripts/update.sh
