6d15f29b14fe9c08a45bc98d4d16e9a21742fe86
Christian Flach <cmfcmf.flach@gmail.com>
2017-08-16 11:14:01 +0200

    Add ability to add affiliate codes to urls
    Requires you to install the PHP mbstring and intl extensions:
    sudo apt install php7.0-mbstring php7.0-intl

---
+++ tests/Twig/AppExtensionTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Tests\Twig;
+
+use AppBundle\Twig\AppExtension;
+
+class AppExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var AppExtension
+     */
+    private $extension;
+
+    public function setUp()
+    {
+        $this->extension = new AppExtension([
+            [
+                'domains' => ['example.com', 'example.org'],
+                'param' => 'aff_id',
+                'code' => 'aff_code'
+            ],
+            [
+                'domains' => ['foo.bar'],
+                'param' => 'aff_id2',
+                'code' => 'aff_code2'
+            ],
+        ]);
+    }
+    /**
+     * @dataProvider urlDataProvider
+     */
+    public function testAddAffiliateCode(string $inputUrl = null, string $expectedOutputUrl = null)
+    {
+        $this->assertEquals($expectedOutputUrl, $this->extension->addAffiliateCode($inputUrl));
+    }
+
+    public function urlDataProvider()
+    {
+        $unrelatedUrl = 'https://www.123.co.uk/foo/bar/../baz/test 123?a=abc&b= aaa&id=1000#foo-bar&x=100';
+
+        return [
+            [null, null],
+            ['', ''],
+            [$unrelatedUrl, $unrelatedUrl],
+            ['http://example.com/foo/bar/../baz/test 123?a=abc&b= aaa&id=1000#foo-bar&x=100', 'http://example.com/foo/bar/../baz/test 123?a=abc&b= aaa&id=1000&aff_id=aff_code#foo-bar&x=100'],
+            ['http://example.org/?aff_id=test', 'http://example.org/?aff_id=aff_code'],
+            ['http://www.foo.bar/?aff_id=test&aff_id2=test2', 'http://www.foo.bar/?aff_id=test&aff_id2=aff_code2'],
+        ];
+    }
+}
