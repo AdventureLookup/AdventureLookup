2452897785fd18043588403035da607dfd43fa14
Robert Burnham <burnhamrobertp@gmail.com>
2017-10-14 19:44:37 -0500

    Move login/register/password reset into a shared directory

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -34,7 +34,7 @@ class SecurityController extends Controller
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', array(
+        return $this->render('shared/login.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
         ));
