2b0165282a82f6173c31486554324d711e428fdd
Christian Flach <cmfcmf.flach@gmail.com>
2018-03-13 19:53:12 +0100

    Disable 'make me your default browser' prompt, which blocked tests
    This only happened the very first time you opened Chrome.

--- scripts/prepare-browser-tests.sh
+++ scripts/prepare-browser-tests.sh
@@ -14,4 +14,4 @@ google-chrome-stable --no-sandbox --headless --remote-debugging-address=0.0.0.0 
 # Xvfb :42 -screen 0 1024x768x16 &
 # x11vnc -display :42 -bg -nopw -listen 0.0.0.0 -xkb
 # DISPLAY=:42 fluxbox &
-# DISPLAY=:42 google-chrome-stable --no-sandbox --start-maximized --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 http://localhost:8003 &
+# DISPLAY=:42 google-chrome-stable --no-sandbox --start-maximized --no-default-browser-check --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 http://localhost:8003 &
