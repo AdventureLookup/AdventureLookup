f0c7a95a1334e076d3fdafb09f6d7dc0e26f0c1e
Christian Flach <cmfcmf.flach@gmail.com>
2020-02-07 17:54:02 +0100

    Update JavaScript dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -11,18 +11,8 @@ Encore
   }))
 
   .addEntry('app', './app/Resources/webpack/js/index.js')
-  .createSharedEntry('vendor', [
-    'jquery',
-    'bootstrap',
-    // TODO: Including the Bootstrap css here duplicates it for some reason, although the docs indicate otherwise:
-    // https://symfony.com/doc/current/frontend/encore/shared-entry.html
-    // If you make it work, don't forget to include the vendor css in the base.html.twig layout:
-    // <link rel="stylesheet" href="{{ asset('assets/vendor.css') }}" />
-    //'bootstrap/scss/bootstrap',
-    'toastr',
-    'selectize',
-    'vanilla-lazyload',
-  ])
+  .createSharedEntry('vendor', './app/Resources/webpack/js/vendor.js')
+  .enableSingleRuntimeChunk()
 
   .enableSassLoader()
   .enablePostCssLoader()
