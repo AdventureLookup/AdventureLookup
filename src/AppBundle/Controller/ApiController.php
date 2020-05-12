17bcda2a44165e053500c12b73cd3f3919a6e4e7
Christian Flach <cmfcmf.flach@gmail.com>
2020-05-12 19:41:45 +0200

    Write tests

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -4,10 +4,9 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Adventure;
 use AppBundle\Entity\AdventureDocument;
-use AppBundle\Entity\ChangeRequest;
-use AppBundle\Entity\Review;
 use AppBundle\Security\AdventureVoter;
 use AppBundle\Service\AdventureSearch;
+use AppBundle\Service\Serializer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -27,17 +26,18 @@ class ApiController extends Controller
      *
      * @param Request $request
      * @param AdventureSearch $adventureSearch
+     * @param Serializer $serializer
      * @return JsonResponse
      */
-    public function indexAction(Request $request, AdventureSearch $adventureSearch)
+    public function indexAction(Request $request, AdventureSearch $adventureSearch, Serializer $serializer)
     {
         list($q, $filters, $page) = $adventureSearch->requestToSearchParams($request);
         list($adventures, $totalNumberOfResults) = $adventureSearch->search($q, $filters, $page);
 
         return new JsonResponse([
             "total_count" => $totalNumberOfResults,
-            "adventures" => array_map(function (AdventureDocument $adventure) {
-                return $this->serializeAdventureDocument($adventure);
+            "adventures" => array_map(function (AdventureDocument $adventure) use ($serializer) {
+                return $serializer->serializeAdventureDocument($adventure);
             }, $adventures)
         ]);
     }
@@ -47,41 +47,13 @@ class ApiController extends Controller
      * @Method("GET")
      *
      * @param Adventure $adventure
+     * @param Serializer $serializer
      * @return JsonResponse
      */
-    public function showAction(Adventure $adventure)
+    public function showAction(Adventure $adventure, Serializer $serializer)
     {
         $this->denyAccessUnlessGranted(AdventureVoter::VIEW, $adventure);
-
-        $reviews = $adventure->getReviews()->map(function (Review $review) {
-            return [
-                "id" => $review->getId(),
-                "is_positive" => $review->isThumbsUp(),
-                "comment" => $review->getComment(),
-                "created_at" => $review->getCreatedAt()->format("c"),
-                "created_by" => $review->getCreatedBy()
-            ];
-        })->toArray();
-
-        $changeRequests = $adventure->getUnresolvedChangeRequests()->map(function (ChangeRequest $changeRequest) {
-            return [
-                "id" => $changeRequest->getId(),
-                "field_name" => $changeRequest->getFieldName(),
-                "comment" => $changeRequest->getComment(),
-                "curator_remarks" => $changeRequest->getCuratorRemarks(),
-                "resolved" => $changeRequest->isResolved(),
-                "updated_at" => $changeRequest->getUpdatedAt()->format("c"),
-                "updated_by" => $changeRequest->getUpdatedBy(),
-                "created_at" => $changeRequest->getCreatedAt()->format("c"),
-                "created_by" => $changeRequest->getCreatedBy()
-            ];
-        })->toArray();
-
-        return new JsonResponse([
-            "adventure" => $this->serializeAdventureDocument(AdventureDocument::fromAdventure($adventure)),
-            "reviews" => $reviews,
-            "change_requests" => $changeRequests
-        ]);
+        return new JsonResponse($serializer->serializeAdventureWithReviewsAndUnresolvedChangeRequests($adventure));
     }
 
 
@@ -94,39 +66,5 @@ class ApiController extends Controller
     public function docsAction() {
 
         return $this->render("api/docs.html.twig");
-    }
-
-    /**
-     * @param AdventureDocument $adventure
-     * @return array
-     */
-    private function serializeAdventureDocument(AdventureDocument $adventure): array
-    {
-        return [
-            "id" => $adventure->getId(),
-            "title" => $adventure->getTitle(),
-            "description" => $adventure->getDescription(),
-            "slug" => $adventure->getSlug(),
-            "authors" => $adventure->getAuthors(),
-            "edition" => $adventure->getEdition(),
-            "environments" => $adventure->getEnvironments(),
-            "items" => $adventure->getItems(),
-            "publisher" => $adventure->getPublisher(),
-            "setting" => $adventure->getSetting(),
-            "common_monsters" => $adventure->getCommonMonsters(),
-            "boss_monsters" => $adventure->getBossMonsters(),
-            "min_starting_level" => $adventure->getMinStartingLevel(),
-            "max_starting_level" => $adventure->getMaxStartingLevel(),
-            "starting_level_range" => $adventure->getStartingLevelRange(),
-            "num_pages" => $adventure->getNumPages(),
-            "found_in" => $adventure->getFoundIn(),
-            "part_of" => $adventure->getPartOf(),
-            "official_url" => $adventure->getLink(),
-            "thumbnail_url" => $adventure->getThumbnailUrl(),
-            "soloable" => $adventure->isSoloable(),
-            "has_pregenerated_characters" => $adventure->hasPregeneratedCharacters(),
-            "has_tactical_maps" => $adventure->isTacticalMaps(),
-            "has_handouts" => $adventure->isHandouts()
-        ];
     }
 }
