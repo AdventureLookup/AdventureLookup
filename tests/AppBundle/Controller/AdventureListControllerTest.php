52e1cb44efd23405f8bb96f9d6a2d3d9fe7b724d
Wigginns <3180645+Wigginns@users.noreply.github.com>
2020-05-20 10:06:09 -0600

    Add initial test file

---
+++ tests/AppBundle/Controller/AdventureListControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use AppBundle\Entity\Adventure;
+use Tests\Fixtures\AdventureData;
+use Tests\WebTestCase;
+
+class AdventureControllerTest extends WebTestCase
+{
+    
+}
