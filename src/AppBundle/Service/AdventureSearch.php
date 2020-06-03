499d98661e000f3caa386ec4445cdd0ca8366762
Christian Flach <cmfcmf.flach@gmail.com>
2020-06-03 10:24:22 +0200

    Use ElasticSearch instead of DB to query random adventure
    This avoids incompatibilities between PostgreSQL and MySQL.

--- src/AppBundle/Service/AdventureSearch.php
+++ src/AppBundle/Service/AdventureSearch.php
@@ -138,7 +138,7 @@ class AdventureSearch
      *
      * @return array
      */
-    public function search(string $q, array $filters, int $page, string $sortBy, string $seed)
+    public function search(string $q, array $filters, int $page, string $sortBy, string $seed, int $perPage = self::ADVENTURES_PER_PAGE)
     {
         if ($page < 1 || $page * self::ADVENTURES_PER_PAGE > 5000) {
             throw new BadRequestHttpException();
@@ -221,8 +221,8 @@ class AdventureSearch
             'index' => $this->indexName,
             'body' => [
                 'query' => $query,
-                'from' => self::ADVENTURES_PER_PAGE * ($page - 1),
-                'size' => self::ADVENTURES_PER_PAGE,
+                'from' => $perPage * ($page - 1),
+                'size' => $perPage,
                 // Also return aggregations for all fields, i.e. min/max for integer fields
                 // or the most common strings for string fields.
                 'aggs' => $this->fieldAggregations(),
